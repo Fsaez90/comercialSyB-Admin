a912d0ae4b85035f211e239e9fc0ce9f0ec5ae0a
Fsaez90 <saezz.f@gmail.com>
2023-06-14 13:53:59 +0200

    fix typo

--- admin/src/components/Home/Ingreso.js
+++ admin/src/components/Home/Ingreso.js
@@ -306,7 +306,7 @@ async function crearOrden(e) {
             <label for="categoría">Categoría:</label>
           <select onChange={(e) => setCategoria(e.target.value)} id="categoria" required>
             <option value="" disabled selected>Seleccionar</option>
-            <option value="Ocasional">Ocacional</option>
+            <option value="Ocasional">Ocasional</option>
             <option value="Semiprofesional">Semiprofesional</option>
             <option value="Profesional">Profesional</option>
           </select>
